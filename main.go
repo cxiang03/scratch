862fbfd3599291717d39e48911f7e0a75435465b
Caleb Xiang <caleb.xiangkh@shopee.com>
2023-05-12 12:27:26 +0800

    blank

--- main.go
+++ main.go
@@ -2,29 +2,6 @@ package main
 
 import "log"
 
-type A struct {
-	ID uint32
-}
-
-func (a *A) GetID() uint32 {
-	return a.ID
-}
-
-type entity interface {
-	GetID() uint32
-}
-
-func ids(entities []entity) []uint32 {
-	ids := make([]uint32, len(entities))
-	for i, e := range entities {
-		ids[i] = e.GetID()
-	}
-	return ids
-}
-
 func main() {
-	a1, a2 := A{ID: 1}, A{ID: 2}
-	entities := []A{a1, a2}
-	log.Println(ids(entities))
-	log.Println("hello")
+	log.Println("hola")
 }
