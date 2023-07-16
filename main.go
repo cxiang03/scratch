77f36e58f108100bc2533eb3fc5cfd1e57abdd72
Caleb Xiang <caleb.xiangkh@shopee.com>
2023-07-16 21:24:55 +0800

    go

--- main.go
+++ main.go
@@ -1,33 +1,7 @@
 package main
 
-type A interface {
-	Foo() string
-}
-
-type B interface {
-	Foo() string
-	Bar() string
-}
-
-func takeA(a A) {
-	a.Foo()
-}
-
-type C struct{}
-
-func NewC() B {
-	return &C{}
-}
-
-func (c *C) Foo() string {
-	return ""
-}
-
-func (c *C) Bar() string {
-	return ""
-}
+import "log"
 
 func main() {
-	c := NewC()
-	takeA(c)
+	log.Println("hello")
 }
