0b23f172ffa248197247d9c3ba722b6fdc9c0522
Caleb Xiang <caleb.xiangkh@shopee.com>
2023-08-03 14:00:49 +0800

    include golangci-lint

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+//nolint:gosec // test
+const (
+	endpoint  = "proxy.uss.s3.test.shopee.io"
+	accessKey = "60086901"
+	secretKey = "jTfWrNlmQOJbTDAaqIyKSkAkRGKSvtxH"
+)
 
 func main() {
-	log.Println("hello")
+	s3, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
+		Secure: false,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	policy, err := s3.GetBucketPolicy(context.Background(), "scratch")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(policy)
 }
